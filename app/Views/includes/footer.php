3a59e74e11fcf83997727a0c3fb29cfcda4f8873
Akhil-Techsoft <techsofttest@gmail.com>
2024-01-24 11:43:43 +0530

    Discount limit fix

--- app/Views/includes/footer.php
+++ app/Views/includes/footer.php
@@ -92,6 +92,19 @@
 
       });
 
+
+      function MinMax(el) {
+        if (el.value != "") {
+            if (parseInt(el.value) < parseInt(el.min)) {
+            el.value = el.min;
+            }
+            if (parseInt(el.value) > parseInt(el.max)) {
+            el.value = el.max;
+            }
+        }
+        }
+
+
     </script>
 
 
