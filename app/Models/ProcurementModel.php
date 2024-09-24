222bd925db3ce1b435b42812a20ebf628fef7b6f
shonfrancis <shonfrancis13@gmail.com>
2024-09-24 10:13:51 +0530

    procuremnt model new

--- app/Models/ProcurementModel.php
+++ app/Models/ProcurementModel.php
@@ -11,43 +11,42 @@ class ProcurementModel extends Model
 
     protected $db;
 
-    public function FetchWhereJoinGp($table,$cond,$joins,$group_by_col){
+    public function FetchWhereJoinGp($table, $cond, $joins, $group_by_col)
+    {
 
         $query = $this->db->table($table);
-        
-        if(!empty($joins))
 
-        foreach($joins as $join)
-        {
-            $table2 = $table;
-            if(!empty($join['table2']))
-            {
-            $table2 = $join['table2'];
+        if (!empty($joins))
+
+            foreach ($joins as $join) {
+                $table2 = $table;
+                if (!empty($join['table2'])) {
+                    $table2 = $join['table2'];
+                }
+                $query->join($join['table'], '' . $join['table'] . '.' . $join['pk'] . ' = ' . $table2 . '.' . $join['fk'] . '', 'left');
             }
-            $query->join($join['table'], ''.$join['table'].'.'.$join['pk'].' = '.$table2.'.'.$join['fk'].'', 'left');
-        }
 
         $query->where($cond);
 
-        $query->groupBy($table. '.' . $group_by_col);
-       
+        $query->groupBy($table . '.' . $group_by_col);
+
         $result = $query->get()->getResult();
-        
+
         return $result;
     }
 
-  
-    public function FetchWhereNotIn($table,$cond,$id_coloum,$id)
+
+    public function FetchWhereNotIn($table, $cond, $id_coloum, $id)
     {
         $query = $this->db->table($table)
-        
-        ->select('*')
 
-        ->whereNotIn($id_coloum,(array)$id)
+            ->select('*')
 
-        ->where($cond)
+            ->whereNotIn($id_coloum, (array)$id)
 
-        ->get();
+            ->where($cond)
+
+            ->get();
 
         //echo $this->db->getLastQuery();
 
@@ -55,29 +54,27 @@ class ProcurementModel extends Model
 
         return $query->getResult();
     }
-    
-    
 
-    public function CheckData($from_date,$from_date_col,$to_date,$to_date_col,$data1,$data1_col,$data2,$data2_col,$data3,$data3_col,$data4,$data4_col,$table,$joins,$group_by_col,$joins1)
-    {   
+
+
+    public function CheckData($from_date, $from_date_col, $to_date, $to_date_col, $data1, $data1_col, $data2, $data2_col, $data3, $data3_col, $data4, $data4_col, $table, $joins, $group_by_col, $joins1)
+    {
         $query = $this->db->table($table)
-        ->select('*');
+            ->select('*');
         // Join additional tables if specified
-        if(!empty($joins))
-        {
-            foreach($joins as $join) 
-            {
+        if (!empty($joins)) {
+            foreach ($joins as $join) {
                 $query->join($join['table'], $join['table'] . '.' . $join['pk'] . ' = ' . $table . '.' . $join['fk'], 'left');
             }
         }
 
         if (!empty($from_date)) {
-           
-            $query->where($from_date_col.' >=', $from_date);
+
+            $query->where($from_date_col . ' >=', $from_date);
         }
 
         if (!empty($to_date)) {
-           
+
             $query->where($from_date_col . ' <=', $to_date);
         }
 
@@ -97,21 +94,19 @@ class ProcurementModel extends Model
             $query->like($data4_col, $data4);
         }
 
-        if(!empty($join))
-        {
-            $query->groupBy($table. '.' . $group_by_col);
-
+        if (!empty($join)) {
+            $query->groupBy($table . '.' . $group_by_col);
         }
-        
-        
+
+
         $result = $query->get()->getResult();
 
         // Get the last executed query
         //echo $this->db->getLastQuery();
-        
+
         //return $result;
 
-      
+
 
         /*$i = 0;
         foreach ($result as $res) {
@@ -121,15 +116,66 @@ class ProcurementModel extends Model
         }*/
 
         return $result;
- 
-
     }
-  
 
 
 
+    // NewAddition
+    public function AccHeadBalance($account)
+    {
+        // Ensure the account and date are sanitized or properly escaped to prevent SQL injection
+        $account = $this->db->escape($account);
+
+
+        // Create the SQL query
+        $query = $this->db->query(
+            '
+                SELECT 
+                    SUM(tran_credit) - SUM(tran_debit) AS total_balance 
+                FROM 
+                    ' . $this->db->getPrefix() . 'master_transactions 
+                WHERE                    
+                    tran_account = ' . $account
+        );
+
+        // Fetch the result
+        $result = $query->getRow();
+
+        // Return the total balance if the result exists, otherwise return 0
+        return $result ? $result->total_balance : 0;
+    }
+
+
+
+
+    public function FetchFixedPurchases($vendor)
+    {
+        // Select the table and columns
+        $query = $this->db->table('pro_purchase_voucher');
+
+        // Summing pv_total from the pro_purchase_voucher
+        $query->select('SUM(pvp_amount) AS total', FALSE);
+
+        // Joining pro_purchase_voucher_prod table
+        $query->join('pro_purchase_voucher_prod', 'pro_purchase_voucher_prod.pvp_reffer_id = pro_purchase_voucher.pv_id', 'left');
+
+        // Joining accounts_charts_of_accounts table
+        $query->join('accounts_charts_of_accounts', 'accounts_charts_of_accounts.ca_id = pro_purchase_voucher_prod.pvp_debit', 'left');
+
+        // Filtering by vendor ID
+        $query->where('ca_account_id', $vendor);
+
+        // Get the result row
+        $result = $query->get()->getRow();
+
+        // Handling potential null results
+        $max = isset($result->total) ? $result->total : 0;
+
+        // Debugging: Show the last query for verification
+        // echo $this->db->getLastQuery();
+        // exit;
+
+        // Return the max total or 0 if no valid result
+        return $max > 0 ? $max : 0;
+    }
 }
-
-
-
-
