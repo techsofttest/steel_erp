0270758452a263b37088a99ad2ba526764583d77
Akhil-Techsoft <techsofttest@gmail.com>
2025-01-31 11:35:50 +0530

    URi changed

--- app/Views/accounts/sub_header.php
+++ app/Views/accounts/sub_header.php
@@ -1,6 +1,9 @@
-<?php  $uri = new \CodeIgniter\HTTP\URI(current_url());?>
+<?php  
+//$uri = new \CodeIgniter\HTTP\URI(current_url());
+$uri = service('request')->uri;
+?>
 
-<?php if($uri->getSegment(3)!="Reports") { ?>
+<?php if($uri->getSegment(2)!="Reports") { ?>
 
 <div class="row">
 
@@ -15,30 +18,30 @@
 
 <ul class="nav nav-tabs nav-border-top nav-border-top-primary mb-3" role="tablist">
     <li class="nav-item waves-effect waves-light">
-        <a class="nav-link <?php if($uri->getSegment(3)=="AccountHead") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/AccountHead" >Account Head</a>
+        <a class="nav-link <?php if($uri->getSegment(2)=="AccountHead") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/AccountHead" >Account Head</a>
     </li>
     <li class="nav-item waves-effect waves-light">
-        <a class="nav-link <?php if($uri->getSegment(3)=="ChartsOfAccounts") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/ChartsOfAccounts">Charts of Accounts</a>
+        <a class="nav-link <?php if($uri->getSegment(2)=="ChartsOfAccounts") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/ChartsOfAccounts">Charts of Accounts</a>
     </li>
     <li class="nav-item waves-effect waves-light">
-        <a class="nav-link <?php if($uri->getSegment(3)=="Receipts") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/Receipts">Receipts</a>
+        <a class="nav-link <?php if($uri->getSegment(2)=="Receipts") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/Receipts">Receipts</a>
     </li>
     <li class="nav-item waves-effect waves-light">
-        <a class="nav-link <?php if($uri->getSegment(3)=="Payments") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/Payments">Payments</a>
+        <a class="nav-link <?php if($uri->getSegment(2)=="Payments") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/Payments">Payments</a>
     </li>
     <li class="nav-item waves-effect waves-light">
-        <a class="nav-link <?php if($uri->getSegment(3)=="JournalVouchers") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/JournalVouchers">Journal Vouchers</a>
+        <a class="nav-link <?php if($uri->getSegment(2)=="JournalVouchers") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/JournalVouchers">Journal Vouchers</a>
     </li>
     <li class="nav-item waves-effect waves-light">
-        <a class="nav-link <?php if($uri->getSegment(3)=="PettyCashVoucher") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/PettyCashVoucher" >Petty Cash Voucher</a>
-    </li>
-
-    <li class="nav-item waves-effect waves-light">
-        <a class="nav-link <?php if($uri->getSegment(3)=="BankRec") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/BankRec" >Bank Reconciliation</a>
+        <a class="nav-link <?php if($uri->getSegment(2)=="PettyCashVoucher") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/PettyCashVoucher" >Petty Cash Voucher</a>
     </li>
 
     <li class="nav-item waves-effect waves-light">
-    <a class="nav-link <?php if($uri->getSegment(3)=="Reports") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/Reports/Ledger" >Reports</a>
+        <a class="nav-link <?php if($uri->getSegment(2)=="BankRec") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/BankRec" >Bank Reconciliation</a>
+    </li>
+
+    <li class="nav-item waves-effect waves-light">
+    <a class="nav-link <?php if($uri->getSegment(2)=="Reports") {echo "active" ;} ?>" href="<?= base_url(); ?>Accounts/Reports/Ledger" >Reports</a>
     </li>
 </ul>
 
