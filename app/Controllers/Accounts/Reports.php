ec0442a1a08341ba0a9595817c51ab49a1b8cb20
Akhil-Techsoft <techsofttest@gmail.com>
2025-03-13 13:29:26 +0530

    Convert remaining balance to positive, Statement of accounts

--- app/Controllers/Accounts/Reports.php
+++ app/Controllers/Accounts/Reports.php
@@ -2138,7 +2138,7 @@ class Reports extends BaseController
 
                 <tr>
                 
-                <td style="border-top:2px solid;border-bottom:2px solid;" align="left"><b>Net Amount Due : '.currency_to_words($remaining_balance).'</b></td>
+                <td style="border-top:2px solid;border-bottom:2px solid;" align="left"><b>Net Amount Due : '.currency_to_words(abs($remaining_balance)).'</b></td>
 
                 <td style="border-top:2px solid;border-bottom:2px solid;" align="right"><b>'.format_currency(abs($remaining_balance)).'</b></td>
 
