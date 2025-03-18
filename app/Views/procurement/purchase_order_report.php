d05fdb45136f503bf7b43a270a6be3824debbc22
shonfrancis <shonfrancis13@gmail.com>
2025-03-18 15:13:13 +0530

    pro alignments

--- app/Views/procurement/purchase_order_report.php
+++ app/Views/procurement/purchase_order_report.php
@@ -247,7 +247,7 @@
                                                     <th class="text-center" style="white-space: nowrap;width:80px">Quantity</th>
                                                     <th class="text-end" style="white-space: nowrap;width:80px">Rate</th>
                                                     <th class="text-end" style="white-space: nowrap;width:80px">Discount</th>
-                                                    <th class="text-center" style="white-space: nowrap;width:80px">Amount</th>
+                                                    <th class="text-end" style="white-space: nowrap;width:80px">Amount</th>
                                                 </tr>
                                             </thead>
 
@@ -290,7 +290,7 @@
 
                                                                             <td class="text-end" style="white-space: nowrap;width:80px"><?php echo format_currency($orders->pop_discount); ?>%</td>
 
-                                                                            <td class="text-center" style="white-space: nowrap;width:80px"><?php echo format_currency($orders->pop_amount);
+                                                                            <td class="text-end" style="white-space: nowrap;width:80px"><?php echo format_currency($orders->pop_amount);
                                                                                                         $po_total += $orders->pop_amount; ?></td>
 
                                                                         </tr>
@@ -304,16 +304,16 @@
 
                                                     <tr>
                                                         <th></th>
-                                                        <th></th>
-                                                        <th></th>
-                                                        <th></th>
-                                                        <th></th>
-                                                        <th class="text-center"><?php echo format_currency($total); ?></th>
-                                                        <th></th>
-                                                        <th></th>
-                                                        <th></th>
-                                                        <th></th>
-                                                        <th class="text-center"><?php echo format_currency($po_total); ?></th>
+                                                        <th style="width:70px">Total</th>
+                                                        <th style="width:100px"></th>
+                                                        <th style="width:300px"></th>
+                                                        <th style="width:100px"></th>
+                                                        <th class="text-end" style="width:80px"><?php echo format_currency($total); ?></th>
+                                                        <th style="min-width:500px"></th>
+                                                        <th style="width:80px"></th>
+                                                        <th style="width:80px"></th>
+                                                        <th style="width:80px"></th>
+                                                        <th class="text-center" style="    padding: 10px 0px 0px 10px !important;"><?php echo format_currency($po_total); ?></th>
                                                     </tr>
 
                                                 <?php
