b3ebce205d357cc58474209e1f25f6f694ed8d95
amrithaaniltechsoft <amrithaaniltechsoft@gmail.com>
2025-03-28 14:35:04 +0530

    update crm

--- app/Views/crm/sales-order_to_dn.php
+++ app/Views/crm/sales-order_to_dn.php
@@ -360,7 +360,7 @@
 
 
 
-
+<script src="<?php echo base_url(); ?>public/assets/js/select2.min.js"></script>
 
 <script>
     document.addEventListener("DOMContentLoaded", function(event) {
@@ -488,6 +488,44 @@
                     var page = params.page || 1;
                     return {
                         results: $.map(data.result, function (item) { return {id: item.product_id, text: item.product_details}}),
+                        pagination: {
+                        // THE `10` SHOULD BE SAME AS `$resultCount FROM PHP, it is the number of records to fetch from table` 
+                            more: (page * 10) <= data.total_count
+                        }
+                    };
+                },              
+            }
+         
+        })
+        /*###*/
+
+
+         /*product droup drown search*/
+         $(".sales_order").select2({
+            placeholder: "Select Product",
+            theme : "default form-control- customer_width",
+            dropdownParent: $('#SalesOrderToDn'),
+            ajax: {
+                url: "<?= base_url(); ?>Crm/SalesOrderToDn/FetchSalesOrder",
+                dataType: 'json',
+                delay: 250,
+                cache: false,
+                minimumInputLength: 1,
+                allowClear: true,
+                data: function (params) {
+                    return {
+                        term: params.term,
+                        page: params.page || 1,
+                    };
+                },
+                processResults: function(data, params) {
+                    //console.log(data);
+                    //NO NEED TO PARSE DATA `processResults` automatically parse it
+                    //var c = JSON.parse(data);
+                    //console.log(data);
+                    var page = params.page || 1;
+                    return {
+                        results: $.map(data.result, function (item) { return {id: item.so_id, text: item.so_reffer_no}}),
                         pagination: {
                         // THE `10` SHOULD BE SAME AS `$resultCount FROM PHP, it is the number of records to fetch from table` 
                             more: (page * 10) <= data.total_count
