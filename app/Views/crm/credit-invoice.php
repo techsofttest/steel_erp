5b7d0a1088fee5bc6a37c3f73559cef4c8f014e9
amrithaaniltechsoft <amrithaaniltechsoft@gmail.com>
2025-04-26 14:54:18 +0530

    update pro

--- app/Views/crm/credit-invoice.php
+++ app/Views/crm/credit-invoice.php
@@ -1431,7 +1431,7 @@ span.select2.customer_width, span.select2 {
         /*add section*/    
         $(function() {
             var form = $('#add_form1');
-            
+            var refreshIntervalId; // Declare outside for proper scope
             form.validate({
                 rules: {
                     required: 'required',
@@ -1502,7 +1502,7 @@ span.select2.customer_width, span.select2 {
                     }
                     else
                     {
-                        alertify.error('Please Select Products').delay(3).dismissOthers();
+                        /*alertify.error('Please Select Products').delay(3).dismissOthers();
 
                         $('#blink').each(function() {
                             var elem = $(this);
@@ -1518,7 +1518,32 @@ span.select2.customer_width, span.select2 {
                         setTimeout(function(){
                             clearInterval(refreshIntervalId);
                             
-                        }, 1000)
+                        }, 1000)*/
+
+                        alertify.error('Please Select Products').delay(3).dismissOthers();
+
+                        // Clear any previous blinking
+                        if (refreshIntervalId) clearInterval(refreshIntervalId);
+
+                        $('#blink').css('visibility', 'visible'); // ensure it starts visible
+
+                        refreshIntervalId = setInterval(function() {
+                            var elem = $('#blink');
+                            if (elem.css('visibility') === 'hidden') {
+                                elem.css('visibility', 'visible');
+                            } else {
+                                elem.css('visibility', 'hidden');
+                            }
+                        }, 200);
+
+                        setTimeout(function() {
+                            clearInterval(refreshIntervalId);
+                            $('#blink').css('visibility', 'visible'); // reset to visible
+                        }, 1000);
+
+
+
+                        
                     }
 
 
--- app/Views/crm/sales-return.php
+++ app/Views/crm/sales-return.php
@@ -1453,7 +1453,7 @@ span.select2.customer_width, span.select2 {
         /*add section*/    
         $(function() {
             var form = $('#add_form1');
-            
+            var refreshIntervalId; // Declare outside for proper scope
             form.validate({
                 rules: {
                     required: 'required',
@@ -1511,7 +1511,7 @@ span.select2.customer_width, span.select2 {
                     }
                     else
                     {
-                        alertify.error('Please Select Products').delay(3).dismissOthers();
+                        /*alertify.error('Please Select Products').delay(3).dismissOthers();
 
                         $('#blink').each(function() {
                             var elem = $(this);
@@ -1528,7 +1528,30 @@ span.select2.customer_width, span.select2 {
                             
                             clearInterval(refreshIntervalId);
                             
-                        }, 1000)
+                        }, 1000)*/
+
+                        alertify.error('Please Select Products').delay(3).dismissOthers();
+
+                        // Clear any previous blinking
+                        if (refreshIntervalId) clearInterval(refreshIntervalId);
+
+                        $('#blink').css('visibility', 'visible'); // ensure it starts visible
+
+                        refreshIntervalId = setInterval(function() {
+                            var elem = $('#blink');
+                            if (elem.css('visibility') === 'hidden') {
+                                elem.css('visibility', 'visible');
+                            } else {
+                                elem.css('visibility', 'hidden');
+                            }
+                        }, 200);
+
+                        setTimeout(function() {
+                            clearInterval(refreshIntervalId);
+                            $('#blink').css('visibility', 'visible'); // reset to visible
+                        }, 1000);
+
+
                     }
 
 
