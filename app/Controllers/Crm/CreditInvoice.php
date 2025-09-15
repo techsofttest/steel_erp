84be7a87f8e5047e7fe93f341a95674b5e7c2b83
amrithaaniltechsoft <amrithaaniltechsoft@gmail.com>
2025-09-15 11:39:15 +0530

    update crm

--- app/Controllers/Crm/CreditInvoice.php
+++ app/Controllers/Crm/CreditInvoice.php
@@ -1510,7 +1510,7 @@ class CreditInvoice extends BaseController
 
         $mpdf = new \Mpdf\Mpdf([
             'margin_top' => 73,
-            'margin_bottom' => 15,
+            'margin_bottom' => 10,
             'margin_left' => 5,
             'margin_right' => 5,
             'defaultfooterline' => 0,
