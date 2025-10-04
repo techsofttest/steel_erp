7774c53376ad1495c85b0c8f87657b15102c61d7
amrithaaniltechsoft <amrithaaniltechsoft@gmail.com>
2025-10-04 11:36:15 +0530

    update salesorder

--- app/Controllers/Crm/SalesOrder.php
+++ app/Controllers/Crm/SalesOrder.php
@@ -1693,7 +1693,7 @@ class SalesOrder extends BaseController
                 'setAutoTopMargin'   => 'stretch',
             ]);
 
-           $mpdf->SetAutoPageBreak(true, 50);
+           $mpdf->SetAutoPageBreak(true, 40);
 
             $mpdf->SetTitle($title);
 
