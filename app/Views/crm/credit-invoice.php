5cefab106fcc8c56704245aae0fd5ab20522fd60
amrithaaniltechsoft <amrithaaniltechsoft@gmail.com>
2025-10-24 17:21:25 +0530

    update crm

--- app/Views/crm/credit-invoice.php
+++ app/Views/crm/credit-invoice.php
@@ -511,7 +511,7 @@ span.select2.customer_width, span.select2 {
                                                     <th>Customer</th>
                                                     <th style="width: 85px !important;">Sales Order</th>
                                                     <th style="width: 100px;"> Amount</th>
-                                                    <th>Actions</th>
+                                                    <th style="width: 100px !important">Actions</th>
                                                 </tr>
                                             </thead>
                                             
@@ -1631,8 +1631,15 @@ span.select2.customer_width, span.select2 {
                ],
 
                columnDefs: [
-                { targets: [5], className: "dt-body-center" }
+                { targets: [0,1,2,4,6], className: "dt-body-center" },
+                { targets: [5], className: "dt-body-right" }
             ],
+            headerCallback: function(thead, data, start, end, display) {
+                // use dt-head-center (header-specific) instead of dt-body-center
+                [0,1,2,4,5,6].forEach(function(i) {
+                    $(thead).find('th').eq(i).addClass('dt-head-center');
+                });
+            },
 
                "initComplete": function () {
 
