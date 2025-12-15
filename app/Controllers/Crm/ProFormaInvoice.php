8e87594d0c8e796316d806309e963b8c4b8bebd4
amrithaaniltechsoft <amrithaaniltechsoft@gmail.com>
2025-12-15 16:50:14 +0530

    update performa invoice

--- app/Controllers/Crm/ProFormaInvoice.php
+++ app/Controllers/Crm/ProFormaInvoice.php
@@ -1617,7 +1617,7 @@ class ProFormaInvoice extends BaseController
         
                                         <tr>
                                         
-                                            <td > </td>
+                                            <td width="11%"> </td>
                                             
                                             <td >'.$proforma_invoice->cc_customer_name.'</td>
                                         
