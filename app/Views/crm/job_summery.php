7fd2e0ff3939efba7671ea97558f39a481a8325f
amrithaaniltechsoft <amrithaaniltechsoft@gmail.com>
2025-12-27 16:18:43 +0530

    update job summery report

--- app/Views/crm/job_summery.php
+++ app/Views/crm/job_summery.php
@@ -333,57 +333,57 @@
 
 
                                                         <td colspan="1" align="left" class="p-0">
-    <table>
-        <?php 
-            // initialize
-            
+   <table>
+<?php 
+    // ✅ INITIALIZE (VERY IMPORTANT)
+    $expenses1 = 0;
+    $expenses2 = 0;
+    $expenses3 = 0;
+    $expenses4 = 0;
+    $expenses5 = 0;
 
-            /* PURCHASE VOUCHERS */
-            if(!empty($sales_order->purchase_vouchers)){
-                foreach ($sales_order->purchase_vouchers as $pur_vouch) {  
-                    $expenses1 += $pur_vouch->pv_total;
-                }
-            }
+    /* PURCHASE VOUCHERS */
+    if (!empty($sales_order->purchase_vouchers)) {
+        foreach ($sales_order->purchase_vouchers as $pur_vouch) {  
+            $expenses1 += (float) $pur_vouch->pv_total;
+        }
+    }
 
-            /* PURCHASE RETURN (should subtract) */
-            if(!empty($sales_order->purchase_return_prod)){
-                foreach($sales_order->purchase_return_prod as $pv_prod){  
-                    $expenses2 += $pv_prod->pr_total_amount;
-                }
-            }
+    /* PURCHASE RETURN (subtract) */
+    if (!empty($sales_order->purchase_return_prod)) {
+        foreach ($sales_order->purchase_return_prod as $pv_prod) {  
+            $expenses2 += (float) $pv_prod->pr_total_amount;
+        }
+    }
 
-            /* PETTY CASH */
-            if(!empty($sales_order->petty_cash)){
-                foreach($sales_order->petty_cash as $p_cash){ 
-                    $expenses3 += $p_cash->pci_amount;
-                }
-            }
+    /* PETTY CASH */
+    if (!empty($sales_order->petty_cash)) {
+        foreach ($sales_order->petty_cash as $p_cash) { 
+            $expenses3 += (float) $p_cash->pci_amount;
+        }
+    }
 
-            /* JOURNAL VOUCHER */
-            if(!empty($sales_order->journal_voucher)){
-                foreach($sales_order->journal_voucher as $jour_vouch){  
-                    if(!empty($jour_vouch->ji_debit))  
-                        $expenses4 += $jour_vouch->ji_debit;
+    /* JOURNAL VOUCHER */
+    if (!empty($sales_order->journal_voucher)) {
+        foreach ($sales_order->journal_voucher as $jour_vouch) {  
+            $expenses4 += (float) ($jour_vouch->ji_debit ?? 0);
+            $expenses5 += (float) ($jour_vouch->ji_credit ?? 0);
+        }
+    }
 
-                    if(!empty($jour_vouch->ji_credit)) 
-                        $expenses5 += $jour_vouch->ji_credit;
-                }
-            }
+    /* ✅ FINAL TOTAL EXPENSES */
+    $expenses = ($expenses1 + $expenses3 + $expenses4)
+              - ($expenses2 + $expenses5);
+?>
 
-            /* FINAL TOTAL EXPENSES */
-            //$expenses = ($expenses1 + $expenses3 + $expenses4 + $expenses5) - $expenses2;
-            $expenses = ($expenses1 + $expenses3 + $expenses4)
-          - ($expenses2 + $expenses5);
-        ?>
+    <!-- TOTAL EXPENSES ROW -->
+    <tr>
+        <td style="width:100px" class="text-end">
+            <?= format_currency($expenses); ?>
+        </td>
+    </tr>
 
-        <!-- TOTAL EXPENSES ROW -->
-        <tr style="">
-            <td style="width:100px" class="text-end">
-                <?= format_currency($expenses); ?>
-            </td>
-        </tr>
-
-    </table>
+</table>
 </td>
 
 <!-- NOW OUTSIDE EXPENSE TABLE: GROSS PROFIT COLUMN -->
