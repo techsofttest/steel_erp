c9fde235ac82c312bb661ac78d99e0cd09aebf92
amrithaaniltechsoft <amrithaaniltechsoft@gmail.com>
2026-01-23 16:59:35 +0530

    update job probablility

--- app/Views/crm/work-progress.php
+++ app/Views/crm/work-progress.php
@@ -133,7 +133,7 @@
                             <div class="col-lg-12" style="padding:0px;">
                                 <div class="card">
                                     <div class="card-header align-items-center d-flex">
-                                        <h4 class="card-title mb-0 flex-grow-1">View  Job Summery <?php if(!empty($from_dates) && !empty($to_dates)){?>(<?php echo $from_dates;?> To <?php echo $to_dates;?>)<?php } ?></h4>
+                                        <h4 class="card-title mb-0 flex-grow-1">View  Work In Progress <?php if(!empty($from_dates) && !empty($to_dates)){?>(<?php echo $from_dates;?> To <?php echo $to_dates;?>)<?php } ?></h4>
                                         
                                         <form method="POST" target="_blank">
                                             <input type="hidden" name="pdf" value="1">
