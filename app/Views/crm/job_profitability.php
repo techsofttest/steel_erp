86454abff1d2e6eaa3507fc9bec153094cc23973
amrithaaniltechsoft <amrithaaniltechsoft@gmail.com>
2026-02-05 09:51:07 +0530

    update job_probability

--- app/Views/crm/job_profitability.php
+++ app/Views/crm/job_profitability.php
@@ -301,14 +301,14 @@ if (!empty($sales_order->purchase_vouchers)) {
     <?php if (!empty($sales_order->purchase_vouchers)) { ?>
         <?php for ($k = 1; $k < $total_ref_count; $k++) { ?>
            <br>
-            <span style="display: inline-block;height: 25px;">tesst</span>
+            <span style="display: inline-block;height: 35px;">tesst</span>
         <?php } ?>
     <?php } ?>
 
     <?php if (!empty($vendor_names)) { ?>
         <?php foreach (array_keys($vendor_names) as $vendor) { ?>
             <br>
-            <span style="display: inline-block;height: 25px;"><?= $vendor; ?></span>
+            <span style="display: inline-block;height: 35px;"><?= $vendor; ?></span>
         <?php } ?>
     <?php } ?>
 
