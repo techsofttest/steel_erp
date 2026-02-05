348da4608e8bc750bad9013e68f7ab968667dde4
amrithaaniltechsoft <amrithaaniltechsoft@gmail.com>
2026-02-05 09:42:52 +0530

    update job_probability

--- app/Views/crm/job_profitability.php
+++ app/Views/crm/job_profitability.php
@@ -296,7 +296,7 @@ if (!empty($sales_order->purchase_vouchers)) {
 
                                          <td style="width:300px; word-wrap: break-word; white-space: normal;">
 
-    <span style="display: inline-block;height: 33px;"><?= $sales_order->cc_customer_name; ?><?php echo $total_ref_count; ?></span>
+    <span style="display: inline-block;height: 33px;"><?= $sales_order->cc_customer_name; ?>-<?php echo $total_ref_count; ?></span>
 
     <?php if (!empty($sales_order->purchase_vouchers)) { ?>
         <?php for ($k = 1; $k < $total_ref_count; $k++) { ?>
