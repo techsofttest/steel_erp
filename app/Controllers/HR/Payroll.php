371f7682b4c0a926ad49212e2da06412d2908e2a
Akhil-Techsoft <techsofttest@gmail.com>
2026-03-26 11:57:14 +0530

    Fix payroll calculation

--- app/Controllers/HR/Payroll.php
+++ app/Controllers/HR/Payroll.php
@@ -866,10 +866,10 @@ class Payroll extends BaseController
 
             $emp_journal ="";
 
-            foreach($timesheets as $ts)
-            {
+           
 
-                //Salary And Deductions
+                    //Salary And Deductions
+                    /*
                     $basic_salary=0;
                     $total_leave=0;
                     $total_ot=0;
@@ -904,7 +904,7 @@ class Payroll extends BaseController
 
                     $leave = $ts->ts_cur_month_leave+$ts->ts_cur_month_unpaid_leave+$ts->ts_current_month_vacation;
 
-                    $basic_salary+=$ts->ts_cur_month_basic_salary+$ts->ts_food_allowance+$ts->ts_other_allowance;
+                    $basic_salary+=$ts->ts_cur_month_basic_salary+$ts->ts_food_allowance+$ts->ts_other_allowance-$leave;
 
                     $total_ot+=$ot;
 
@@ -920,10 +920,64 @@ class Payroll extends BaseController
 
                     //$other_allow+=$ts->ts_other_allowance;
 
-                    $total_salary+=$ts->ts_cur_month_salary+$ts->ts_food_allowance+$ts->ts_other_allowance-$leave;
+                    $total_salary+=$ts->ts_cur_month_salary+$ts->ts_food_allowance+$ts->ts_other_allowance;
 
                     }
-        }
+                    */
+
+
+                    $basic_salary=0;
+                    $total_leave=0;
+                    $total_ot=0;
+
+                    $house_rent_allow=0;
+                    $transport_allow=0;
+                    $telephone_allow=0;
+
+                    $total_salary=0;
+
+                    $staff_salary=0;
+                    $salaries_wages=0;
+
+                    foreach($timesheets as $ts)
+                    {
+
+                        $ot = $ts->ts_cur_month_normal_ot + $ts->ts_cur_month_friday_ot;
+
+                        $leave = $ts->ts_cur_month_leave
+                                + $ts->ts_cur_month_unpaid_leave
+                                + $ts->ts_current_month_vacation;
+
+                        $ts_basic_salary = $ts->ts_cur_month_basic_salary
+                                        + $ts->ts_food_allowance
+                                        + $ts->ts_other_allowance
+                                        - $leave;
+
+                        if($ts->emp_division==2)
+                        {
+                            $staff_salary += $ts_basic_salary;
+                        }
+
+                        if($ts->emp_division==1)
+                        {
+                            $salaries_wages += $ts_basic_salary;
+                        }
+
+                        $basic_salary += $ts_basic_salary;
+
+                        $total_ot += $ot;
+
+                        $total_leave += $leave;
+
+                        $house_rent_allow += $ts->ts_house_rent_allowance;
+
+                        $transport_allow += $ts->ts_transportation_allowance;
+
+                        $telephone_allow += $ts->ts_telephone_allowance;
+
+                        $total_salary += $ts->ts_cur_month_salary;
+                    }
+       
 
         $insert_payroll['pr_month'] = $month;
         $insert_payroll['pr_year'] = $year;
