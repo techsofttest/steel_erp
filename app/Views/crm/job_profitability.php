037121df5a57050be7f9c4d9c690b3f227d4e052
amrithaaniltechsoft <amrithaaniltechsoft@gmail.com>
2026-03-26 16:39:27 +0530

    update job_probability

--- app/Views/crm/job_profitability.php
+++ app/Views/crm/job_profitability.php
@@ -286,7 +286,8 @@ $printedPV = [];
 if (!empty($sales_order->purchase_vouchers)) {
     foreach ($sales_order->purchase_vouchers as $pv) {
         if (!empty($pv->pv_reffer_id) && !in_array($pv->pv_reffer_id, $printedPV)) {
-            $vendor_names[$pv->cc_customer_name] = true; 
+            //$vendor_names[$pv->cc_customer_name] = true; 
+            $vendor_names[] = $pv->cc_customer_name;
             $printedPV[] = $pv->pv_reffer_id;
         }
     }
