cfd0e2ea0d84a0d965eed0c2c6b060836b8a61a2
amrithaaniltechsoft <amrithaaniltechsoft@gmail.com>
2026-04-20 15:59:22 +0530

    update purchase voucher analysis

--- app/Views/procurement/lpo_pv_report.php
+++ app/Views/procurement/lpo_pv_report.php
@@ -952,7 +952,7 @@
                         purchaseorder: $('.lpo_ref').val()
                     };
                 },
-                processResults: function(data, params) {
+                /*processResults: function(data, params) {
                     var page = params.page || 1;
                     return {
                         results: $.map(data.result, function(item) {
@@ -965,7 +965,29 @@
                             more: (page * 10) <= data.total_count
                         }
                     };
-                },
+                },*/
+                processResults: function (data, params) {
+                    var page = params.page || 1;
+                    let seen = {};
+                    let results = [];
+                    $.each(data.result, function (i, item) {
+                        // ✅ prevent duplicate sales order
+                        if (!seen[item.product_id]) {
+                            seen[item.product_id] = true;
+
+                            results.push({
+                                id: item.product_id,
+                                text: $.trim(item.product_details)
+                            });
+                        }
+                    });
+                    return {
+                        results: results,
+                        pagination: {
+                            more: (page * 10) <= data.total_count
+                        }
+                    };
+                }
             }
         });
 
