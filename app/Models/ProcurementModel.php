ec1fb2e57d520e9e3f59f74e27f54d9e47d8a0c6
amrithaaniltechsoft <amrithaaniltechsoft@gmail.com>
2026-04-20 15:51:27 +0530

    purchase voucher analysis

--- app/Models/ProcurementModel.php
+++ app/Models/ProcurementModel.php
@@ -2920,7 +2920,7 @@ class ProcurementModel extends Model
 
         $result = $query->get()->getResult();
 
-        echo $this->db->getLastQuery(); exit();
+        echo $this->db->getLastQuery(); 
        
         return $result;
         
